ae3641f2d1924d59f3f5fc10f4890637a838e978
omeraktay <omer.aktay@live.com>
2025-01-14 22:17:35 -0800

    I added book inventory

---
+++ book_inventory/server.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import morgan from 'morgan';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+
+const app = express();
+const PORT = 2121; 
+
+app.use(express.json());
+app.use(morgan('dev'));
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname,' public')));
+
+app.get('/', (req, res) => {
+    const dataPath = path.join(__dirname, 'data', 'inventory.json');
+    const inventory = JSON.parse(fs.readFileSync(dataPath, 'utf-8'));
+    res.render('index', { inventory });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
