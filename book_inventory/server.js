221e177fa9228f9958b8657954f6222587bee3d3
omeraktay <omer.aktay@live.com>
2025-01-14 22:25:26 -0800

    I fixed the mistake

--- book_inventory/server.js
+++ book_inventory/server.js
@@ -15,7 +15,7 @@ const __dirname = path.dirname(__filename);
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(path.join(__dirname,' public')));
+app.use(express.static(path.join(__dirname,'public')));
 
 app.get('/', (req, res) => {
     const dataPath = path.join(__dirname, 'data', 'inventory.json');
